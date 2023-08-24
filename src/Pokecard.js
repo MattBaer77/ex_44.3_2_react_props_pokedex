504c43f8e22e35b6b55cf0cdce8cf950fcd91c97
Matthew Baer <matt.baer77@gmail.com>
2023-08-24 00:15:40 -0400

    Pokecard.js and Pokedex.js functioning

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,20 @@
 
 import React from 'react';
 
-const Pokedex = () => {
+const Pokecard = ({name, img, type, exp}) => {
 
-    return "pokecard"
+    return (
+
+        <div>
+
+            <h2>{name}</h2>
+            <img src={img}></img>
+            <h3>Type: {type}</h3>
+            <h3>EXP: {exp}</h3>
+
+        </div>
+
+    )
 
 }
 
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,37 @@
 
-/*
 
-For defaultProps
+import React from 'react';
+import Pokecard from './Pokecard'
 
-[
+const Pokedex = ({pokemon}) => {
+
+  console.log(pokemon)
+
+  return (
+
+    <div>
+
+      <h1>
+        Pokedex
+      </h1>
+
+      <div>
+
+        {pokemon.map(p => (
+
+          <Pokecard name={p.name} img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} type={p.type} exp={p.base_experience} />
+
+        ))}
+
+      </div>
+
+    </div>
+
+  )
+
+}
+
+Pokedex.defaultProps = {pokemon: [
   {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
   {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
   {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
@@ -12,17 +40,8 @@ For defaultProps
   {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
   {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
   {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-]
+]}
 
-*/
-
-import React from 'react';
-import Pokecard from './Pokecard'
-
-const Pokedex = () => {
-
-    return "pokedex"
-
-}
+console.log(Pokedex.defaultProps)
 
 export default Pokedex;
